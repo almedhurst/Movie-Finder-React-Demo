99dee89a2097a1d7bd4396c7a35fa9260896e251
Pickles <a.l.medhurst+github@outlook.com>
2022-08-22 10:11:16 +0100

    Applied deployment fixes

---
+++ MovieFinder.Web/Controllers/FallbackController.cs
@@ -0,0 +1,13 @@
+﻿using System.Net.Mime;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MovieFinder.Web.Controllers;
+
+public class FallbackController : Controller
+{
+    public IActionResult Index()
+    {
+        return PhysicalFile(Path.Combine(Directory.GetCurrentDirectory(),
+            "wwwroot", "index.html"), MediaTypeNames.Text.Html);
+    }
+}
